faea215649683bd03bfed8eafc3df4ee91ccd236
ffelder <felippafelder@gmail.com>
2018-10-07 22:46:42 +0100

    up to 69

---
+++ src/componentes/App.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+
+import '../css/App.css';
+import Header from './Header';
+import Formulario from './Formulario';
+import Listado from './Listado';
+import{validarPresupuesto} from '../helper';
+import ControlPresupuesto from'./ControlPresupuesto';
+
+class App extends Component {
+
+	state={
+		presupuesto:'',
+		restante:'',
+		gastos:{}
+	}
+
+	componentDidMount(){
+		this.obtenerPresupuesto();
+	}
+	obtenerPresupuesto=()=>{
+		let presupuesto=prompt('Cual es el presupuesto');
+		let resultado = validarPresupuesto(presupuesto);
+		if(resultado){
+		    this.setState({
+		    	presupuesto:presupuesto,
+		    	restante:presupuesto
+		    })
+		}else{
+			this.obtenerPresupuesto();
+		}
+	}
+    
+    //Agregar un nuevo gasto al state
+    agregarGasto=gasto=>{
+    	//tomar una copia del state actual
+    	const gastos={...this.state.gastos};
+    	
+
+    	//agregar el gasto al objeto del state
+    	gastos[`gastos${Date.now()}`]=gasto;
+    	console.log(gastos)
+
+    	//ponerlo en el state
+    	this.setState({
+    		gastos:gastos
+    	}
+
+    		)
+    }
+
+
+  render() {
+    return (
+    	<div className="App container">
+
+    	<Header
+    	titulo='Gasto Semanal'
+
+    	/>
+
+    	
+    	<div className="contenido-principal contenido">
+    	   <div className="row">
+    	      <div className="one-half column">
+                <Formulario
+                agregarGasto={this.agregarGasto}
+
+
+
+                />
+    	      </div>
+    	      <div className="one-half column">
+    	      <Listado
+
+    	      gastos={this.state.gastos}
+
+    	      />
+    	      <ControlPresupuesto
+    	      presupuesto={this.state.presupuesto}
+    	      restante={this.state.restante}
+
+    	      />
+    	        
+    	      </div>
+
+
+    	   </div>
+
+    	</div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/componentes/Restante.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+class Restante extends Component{
+	render(){
+		return(
+			<div className='alert alert-success'>
+			Restante: $ {this.props.restante}
+			</div>
+			)
+	}
+}
+export default Restante;
